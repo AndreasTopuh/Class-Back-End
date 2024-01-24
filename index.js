1a4d784b975226b14bf4ed3489d6af952f175c6d
AndreasTopuh <andreastopuh23@gmail.com>
2024-01-24 19:51:08 +0800

    adding some models

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+////////////////KONDISI DISIMPAN DALAM TRY & CATCH//////////////
+
 const http = require('http');
 const port = 3000;
 
@@ -5,35 +7,32 @@ const member = require('./member.js');
 const users = require('./users.js');
 
 const about = {
-    status: "Success",
-    message: "Response Success",
-    description: "Tugas #2",
-    date: new Date(),
-    data: member
+  status: "Success",
+  message: "Response Success",
+  description: "Exercise #2",
+  date: new Date(),
+  data: member
 };
 
 const server = http.createServer((req, res) => {
     const path = req.url;
 
-    try {
-        if (path === '/') {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/plain');
-            res.write("This is the home page");
-            res.end();
-        } else if (path === '/about') {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.write(JSON.stringify(about));
-            res.end();
-        } else if (path === '/users') {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.write(JSON.stringify(users));
-            res.end();
-        }
-    } catch (error) {
-        // Handle the error by sending a 404 response
+    if (path === '/') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.write("<h1>This is the home page</h1>");
+        res.end();
+    } else if (path === '/about') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify(about));
+        res.end();
+    } else if (path === '/users') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify(users));
+        res.end();
+    } else {
         res.statusCode = 404;
         res.setHeader('Content-Type', 'text/plain');
         res.write('404 Not Found bro hehehe salam dari Andreas');
@@ -44,3 +43,51 @@ const server = http.createServer((req, res) => {
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/ program by Andreas Topuh `);
 });
+
+
+//////////////USING IF BIASA////////////////////////////
+
+// const http = require('http');
+// const port = 3000;
+
+// const member = require('./member.js');
+// const users = require('./users.js');
+
+// const about = {
+//   status: "Success",
+//   message: "Response Success",
+//   description: "Exercise #2",
+//   date: new Date(),
+//   data: member
+// };
+
+// const server = http.createServer((req, res) => {
+//     const path = req.url;
+
+//     if (path === '/') {
+//         res.statusCode = 200;
+//         res.setHeader('Content-Type', 'text/plain');
+//         res.write("This is the home page");
+//         res.end();
+//     } else if (path === '/about') {
+//         res.statusCode = 200;
+//         res.setHeader('Content-Type', 'application/json');
+//         res.write(JSON.stringify(about));
+//         res.end();
+//     } else if (path === '/users') {
+//         res.statusCode = 200;
+//         res.setHeader('Content-Type', 'application/json');
+//         res.write(JSON.stringify(users));
+//         res.end();
+//     } else {
+//         res.statusCode = 404;
+//         res.setHeader('Content-Type', 'text/plain');
+//         res.write('404 Not Found bro hehehe salam dari Andreas');
+//         res.end();
+//     } 
+
+// });
+
+// server.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}/ program by Andreas Topuh `);
+// });
