d1df417293a1732e500a9063b14e421711d4cf25
AndreasTopuh <andreastopuh23@gmail.com>
2024-02-12 15:41:14 +0800

    adding tugas 3

--- index.js
+++ index.js
@@ -1,93 +1,44 @@
-////////////////KONDISI DISIMPAN DALAM TRY & CATCH//////////////
-
-const http = require('http');
+const express = require('express');
+const morgan = require('morgan');
 const port = 3000;
 
-const member = require('./member.js');
-const users = require('./users.js');
+const member = require('./member.js'); 
+const users = require('./users.js'); 
+const app = express();
 
-const about = {
-  status: "Success",
-  message: "Response Success",
-  description: "Exercise #2",
-  date: new Date(),
-  data: member
-};
+// Middleware
+app.use(morgan('dev')); 
 
-const server = http.createServer((req, res) => {
-    const path = req.url;
-
-    if (path === '/') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.write("<h1>This is the home page</h1>");
-        res.end();
-    } else if (path === '/about') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.write(JSON.stringify(about));
-        res.end();
-    } else if (path === '/users') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.write(JSON.stringify(users));
-        res.end();
-    } else {
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/plain');
-        res.write('404 Not Found bro hehehe salam dari Andreas');
-        res.end();
-    }
+app.use((req, res, next) => {
+    // Add your middleware logic here
+    // For example, you can log the request method and URL
+    console.log(`[${req.method}] ${req.url}`);
+    next();
 });
 
-server.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/ program by Andreas Topuh `);
+app.get('/', (req, res) => {
+    res.send('<h1>This is the home page</h1>');
 });
 
+app.get('/about', (req, res) => {
+    const about = {
+        status: "Success",
+        message: "Response Success",
+        description: "Exercise #3 menggunakan EXPRESS.JS dan MORGAN",
+        date: new Date(),
+        data: member
+    };
+    res.json(about);
+});
 
-//////////////USING IF BIASA////////////////////////////
+app.get('/users', (req, res) => {
+    res.json(users);
+});
 
-// const http = require('http');
-// const port = 3000;
+app.use((req, res, next) => {
+    res.status(404).send('Not Found');
+});
 
-// const member = require('./member.js');
-// const users = require('./users.js');
-
-// const about = {
-//   status: "Success",
-//   message: "Response Success",
-//   description: "Exercise #2",
-//   date: new Date(),
-//   data: member
-// };
-
-// const server = http.createServer((req, res) => {
-//     const path = req.url;
-
-//     if (path === '/') {
-//         res.statusCode = 200;
-//         res.setHeader('Content-Type', 'text/plain');
-//         res.write("This is the home page");
-//         res.end();
-//     } else if (path === '/about') {
-//         res.statusCode = 200;
-//         res.setHeader('Content-Type', 'application/json');
-//         res.write(JSON.stringify(about));
-//         res.end();
-//     } else if (path === '/users') {
-//         res.statusCode = 200;
-//         res.setHeader('Content-Type', 'application/json');
-//         res.write(JSON.stringify(users));
-//         res.end();
-//     } else {
-//         res.statusCode = 404;
-//         res.setHeader('Content-Type', 'text/plain');
-//         res.write('404 Not Found bro hehehe salam dari Andreas');
-//         res.end();
-//     } 
-
-// });
-
-// server.listen(port, () => {
-//     console.log(`Server running at http://localhost:${port}/ program by Andreas Topuh `);
-// });
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
